1b1e593ebb2be8fe80460059e2950dc242145776
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2023-10-18 20:57:33 +0530

    folder shuffling

--- client/src/components/wallet/Wallet.jsx
+++ client/src/components/wallet/Wallet.jsx
@@ -14,7 +14,7 @@ const Wallet = ({ children }) => {
   useEffect(() => {
     const init = async () => {
       const web3 = new Web3("HTTP://127.0.0.1:7545");
-      const contractAddress = "0x911d0c60E5Ec6e93E8aE7125313D8f9F4CdF28d1";
+      const contractAddress = "Add your contract address";
       //to create contract instance - abi and contract address
       const contract = new web3.eth.Contract(ABI, contractAddress);
       setState({ web3: web3, contract: contract });
